35e737cc98f5cd66fc41f09e71943d7d6a8ccc55
Zach Leatherman <zachleatherman@gmail.com>
2023-10-30 13:29:38 -0500

    Code cleanup

--- public/demo-currentsrc.js
+++ public/demo-currentsrc.js
@@ -1,23 +1,28 @@
 /* On-off script to highlight active src */
+let images = document.querySelectorAll("img");
+
+function updateActiveSource(img) {
+	if(img.currentSrc) {
+		let u = new URL(img.currentSrc);
+
+		let section = img.closest("section");
+
+		let previousRow = section.querySelector(`tr.currentsrc`);
+		previousRow?.classList.remove("currentsrc");
+
+		let currentSrcRow = section.querySelector(`tr[data-url="${u.pathname}"]`);
+		currentSrcRow?.classList.add("currentsrc", "loadedsrc");
+	}
+}
+
 if("ResizeObserver" in window) {
 	let resizer = new ResizeObserver((entries) => {
 		for(let entry of entries) {
 			let img = entry.target;
-			if(img.currentSrc) {
-				let u = new URL(img.currentSrc);
-
-				let section = img.closest("section");
-
-				let previousRow = section.querySelector(`tr.currentsrc`);
-				previousRow?.classList.remove("currentsrc");
-
-				let currentSrcRow = section.querySelector(`tr[data-url="${u.pathname}"]`);
-				currentSrcRow?.classList.add("currentsrc", "loadedsrc");
-			}
+			updateActiveSource(img);
 		}
 	});
 
-	let images = document.querySelectorAll("img");
 	for(let i of images) {
 		resizer.observe(i);
 	}
