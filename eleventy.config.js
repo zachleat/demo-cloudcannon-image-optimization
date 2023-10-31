320e4e22a10aa7b5a036d7009ca62312b371a25b
Zach Leatherman <zachleatherman@gmail.com>
2023-10-31 09:40:10 -0500

    code reorg

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,50 +8,40 @@ const IMAGE_OPTIONS = {
 	urlPath: "/optimized/",
 };
 
-async function image(inputFilePath, alt, sizes, preferSvg) {
-	let before = Date.now();
-	let metadata = await Image(inputFilePath, Object.assign({
-		svgShortCircuit: preferSvg ? "size" : false,
-	}, IMAGE_OPTIONS));
-	console.log( "[11ty/eleventy-img]", `${Date.now() - before}ms`, inputFilePath );
-
-	let imageAttributes = {
-		alt,
-		sizes,
-		loading: "eager",
-		decoding: "async",
-		// fetchpriority: "high", // "auto",
-	};
-
-	// You bet we throw an error on a missing alt (alt="" works okay)
-	return Image.generateHTML(metadata, imageAttributes);
-}
-
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addShortcode("image", async (srcFilePath, alt, sizes, preferSvg) => {
+		let inputFilePath = path.join(eleventyConfig.dir.input, srcFilePath);
+		let metadata = await Image(inputFilePath, Object.assign({
+			svgShortCircuit: preferSvg ? "size" : false,
+		}, IMAGE_OPTIONS));
 
-	eleventyConfig.addShortcode("image", (srcFilePath, ...args) => {
-		let filePath = path.join(eleventyConfig.dir.input, srcFilePath);
-		return image(filePath, ...args)
+		return Image.generateHTML(metadata, {
+			alt,
+			sizes,
+			loading: "eager",
+			decoding: "async",
+		});
 	});
-
-
-
-
-
-	// Server
-	eleventyConfig.setServerOptions({
-		domDiff: false,
-	})
-
-	// Ignores
-	eleventyConfig.ignores.add("README.md");
-	eleventyConfig.ignores.add("src/_schemas/*");
 
 	// Components
 	eleventyConfig.addPassthroughCopy({
 		"./public/*": "/",
 		"./node_modules/@zachleat/browser-window/browser-window.js": "/browser-window.js",
 	});
+
+	// Server
+	eleventyConfig.setServerOptions({
+		domDiff: false,
+	});
+
+	// Liquid
+	eleventyConfig.setLiquidOptions({
+		jsTruthy: true
+	});
+
+	// Ignores
+	eleventyConfig.ignores.add("README.md");
+	eleventyConfig.ignores.add("src/_schemas/*");
 
 	// Plugin to show optimized Image file sizes in a nice table.
 	eleventyConfig.addPlugin(require("./11ty/filesize-table.js"), {
